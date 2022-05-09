bcc72d926505dd9d351e4224fcb9ff976f323315
Noah Beij <36964954+thedutchgunslinger@users.noreply.github.com>
2022-05-09 21:21:16 +0200

    new 3d scene

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -7,7 +7,7 @@ console.log(THREE);
 console.log(gsap);
 const COLORS = {
   background: "#8888ff",
-  light: "#ffffff",
+  light: "##526b9c",
   sky: "#aaaaff",
   ground: "#88ff88",
 };
@@ -17,7 +17,7 @@ let size = { width: 0, height: 0 };
 const PI = Math.PI;
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(COLORS.background);
-scene.fog = new THREE.Fog(COLORS.background, 15, 20);
+scene.fog = new THREE.Fog(COLORS.background, 150, 200);
 const renderer = new THREE.WebGLRenderer({
   antialias: true,
 });
@@ -47,21 +47,25 @@ scene.add(camera);
 
 // --- LIGHTS
 
-const directionalLight = new THREE.DirectionalLight(COLORS.light, 2);
+const directionalLight = new THREE.DirectionalLight(COLORS.light, 1);
 directionalLight.castShadow = true;
 directionalLight.shadow.camera.far = 10;
 directionalLight.shadow.mapSize.set(1024, 1024);
 directionalLight.shadow.normalBias = 0.05;
-directionalLight.position.set(2, 5, 3);
+directionalLight.position.set(2, 1, 2);
 
-scene.add(directionalLight);
+// scene.add(directionalLight);
 
 const hemisphereLight = new THREE.HemisphereLight(
   COLORS.sky,
   COLORS.ground,
   0.5
 );
-scene.add(hemisphereLight);
+
+const light = new THREE.PointLight(COLORS.light, 0.5, 10);
+light.position.set(2, 1, 3);
+scene.add(light);
+// scene.add(hemisphereLight);
 
 
 // --- ON RESIZE
@@ -93,10 +97,10 @@ tick();
 let modal;
 
 loader.load(
-  "./assets/js/noah.glb",
+  "./assets/js/test.glb",
   function (gltf) {
       modal = gltf.scene;
-      modal.position.set(0, 0, -1);
+      modal.position.set(0.7, 0, -3);
     scene.add(modal);
   },
   undefined,
@@ -140,13 +144,15 @@ loader.load(
      ease: "power2.inOut",
    },
  });
-        tl.to(modal.position, { x: -1 }, section);
+        tl.to(camera.position, { x: -2 }, section);
         section += 1;
-        tl.to(modal.rotation, { y: 1 }, section);
+        // tl.to(modal.scale, { z: 2, x: 2, y: 2 }, section);
+        // section += 1;
+        tl.to(camera.rotation, { y: 1 }, section);
         section += 1;
-        tl.to(modal.scale, { z: 2, x: 2, y: 2 }, section);
-        section += 1;
-        tl.to(modal.position, { z: -10 }, section);
+        // tl.to(camera.position, { z: -10 }, section);
+
+        
 
         renderer.render(scene, camera);
       }
